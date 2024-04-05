40de43625cf601aee20ac7778d0e94d766107da4
Essadike  Elhafiane <eelhafia@e1r7p6.1337.ma>
2024-04-05 00:26:36 +0000

    fix : notification seen

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -10,6 +10,7 @@ import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en';
 import { Span } from "next/dist/trace";
 import { getTimeAgo } from "./timeAgo";
+import { ClassNames } from "@emotion/react";
 
 const PhoneLogo = ()=>{}
 
@@ -32,14 +33,21 @@ type NotificationType = {
     seen: boolean,
 }
 
+
 const Notification = () =>{
     const [notification, setNotification] = useState(false);
     const ref = useClickAway<HTMLDivElement>( () => {
+        if (!notification )
+            return ;
         setNotification(false);
+        setNumberNotf(0);
+
       });
     const [notificationList, setNotificationList] = useState<NotificationType[]>([]);
     const socket = useContext(SocketContext);
     const [timeAgo, setTimeAgo] = useState<TimeAgo | null>(null);
+    const [numberNotf, setNumberNotf] = useState<number>(0);
+
     useEffect(() => {
         setTimeAgo(getTimeAgo());
         // No need for cleanup to set to null, but you can keep it if it suits your use case
@@ -48,6 +56,14 @@ const Notification = () =>{
         };
    }, []);
 
+   const NotificationsSeen = async () => {
+        const res = await axios.get(process.env.NEST_API + "/user/NotificationsSeen" , {
+            withCredentials: true,
+            }
+        )
+        // setNotificationList(data);
+        
+   }
 
     useEffect(() => {
         
@@ -56,36 +72,43 @@ const Notification = () =>{
                 withCredentials: true,
                 }
             )
-            setNotificationList(res.data.notifications);
+            const data:  NotificationType[] = res.data.notifications;
+            setNotificationList(data);
+            setNumberNotf(data.filter((data) => !data.seen).length);
         }
         fetchNotification();
 
         socket?.on("Notification", (data: NotificationType) => {
             setNotificationList((prev) =>prev.length? [data,  ...prev ]: [data]);
+            setNumberNotf((prev) => prev + 1);
         });
         return () => {
             socket?.off("Notification");
         }
     }
-    , [socket]);
+    , [socket, numberNotf]);
     
     return (
-        <>
-        <div className="notification cursor-pointer" onClick={()=> setNotification(!notification)}>
-            {/* <Image className="notificationLogo" src="./homeImages/Ell.svg" alt="logo" width={48} height={48}/> */}
-            {/* <Image className="notificationLogo" src="./homeImages/Ell1.svg" alt="logo" width={42} height={42}/> */}
-            <div className="absolute text-white ml-[30px] mt-[-4px] rounded-full NumNotf">{notificationList?.length > 100 ? '99+' : `${notificationList?.length || '0'}`}</div>
+        <div ref={ref} className="z-100">
+        <div className="notification cursor-pointer" onClick={()=> {
+            notification && setNumberNotf(0);
+            setNotification(!notification)
+            // setNumberNotf(0)
+    
+            NotificationsSeen();
+        }}>
+            <div className="absolute text-white ml-[30px] mt-[-4px] rounded-full NumNotf">{numberNotf> 100 ? '99+' : `${numberNotf|| '0'}`}</div>
             <div className="NtfIcon">
                 <Image  src="./homeImages/Ell2.svg" alt="logo" width={26} height={26}/>
             </div>
 
         </div>
         {notification && 
-            <div className="NtfContainer absolute"  ref={ref}>
+            <div className="NtfContainer absolute"  >
                 {   notificationList?.length > 0 ?
-                    notificationList.map((data, index) => (
+                    notificationList.map((data) => (
                        
-                        <div className="notificationList" key={data.id}>
+                        <div className={data.seen? 'bg-[#23233efc] notificationList' : 'notificationList bg-[#1B1A55]'} key={data.id}>
                             <Image src={data.image} className="rounded-full" alt="image" width={34} height={34}/>
                             <p className="infoNtf"> <span className='userNameSpan'>{data.userName} </span>{data.content}</p>
                             <p className="text-[12px]  absolute mt-[40px] ml-[180px]">{timeAgo?.format(new Date(data.createdAt))}</p>
@@ -95,10 +118,10 @@ const Notification = () =>{
                     <div className="notificationList">
                         <p className="text-sm">No Notification</p>
                     </div>
-                } 
+                }
             </div>
         }
-    </>
+    </div>
     );
 }
 
